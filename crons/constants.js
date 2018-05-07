190d8d6b517100cf1b8e7a1a4d91bad47c1f9588
Gregory Wilson <greg@awnage.com>
2018-05-07 15:56:10 -0400

    misc sms cron files

---
+++ crons/constants.js
@@ -0,0 +1,42 @@
+const PRODUCT_CODES = {
+  'ACT 1x6':'AA',
+  'ACT 6s':'AA',
+  'ACT 2x6':'AB',
+  'ACT 12s':'AB',
+  'ACT 3x6':'AC',
+  'ACT 18s':'AC',
+  'ACT 4x6':'AD',
+  'ACT 24s':'AD',
+  'Malaria Rapid Diagnostic Test Kit':'RD',
+  'RDT':'RD',
+  'RDTs':'RD',
+  'Zinc 10mg':'ZN',
+  'Zinc Tablets':'ZN',
+  'ORS':'RS',
+  'Oral Rehydration Salt':'RS',
+  'Zinc/ORS Co-Pack':'ZR',
+  'Zinc/ORS':'ZR',
+  'Tetracycline Eye Ointment':'TE',
+  'Albendazole':'AL',
+  'Paracetamol':'PA',
+  'Chlorine Tablets':'CH',
+  'Chlorine':'CH',
+  'Amoxicillin Dispersable (125mg)':'AM',
+  'Amoxicillin':'AM',
+  'Amoxicillin Dispersable (250mg)':'AN',
+  'Male Condoms':'MC',
+
+  'Vitamin A 50 IU':'VA',
+  'Vitamin A 100 IU':'VB',
+  'Vitamin A 200 IU':'VC',
+  //  'COC':'CC',
+  //  'Iodine':'ID',
+  //  'Amoxicillin Dispensable Tablets':'AT',
+}
+
+const MONTHLY_SOH_DATASETUID = 'z2slLbjn7PM';
+
+module.exports = {
+  PRODUCT_CODES: PRODUCT_CODES,
+  MONTHLY_SOH_DATASETUID: MONTHLY_SOH_DATASETUID
+}
---
+++ crons/license.md
@@ -0,0 +1,5 @@
+# License
+
+Software written for cStock project by University of Oslo in conjunction with BAO Systems.
+
+This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
